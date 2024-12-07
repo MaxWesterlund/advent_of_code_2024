6c191979ad49cdc97cb70c8b6cb94b5571469c21
MaxWesterlund <max.westerlund3@gmail.com>
2024-12-07 11:38:45 +0100

    day 5 started

---
+++ day_5/part_1.py
@@ -0,0 +1,4 @@
+with open("test_input.txt", "r") as file:
+    # Put rules in dictionary with array that says which numbers are before
+
+    # Go through the sequences and check the numbers after a number with the rules
